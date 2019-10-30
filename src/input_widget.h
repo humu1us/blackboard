9ede9a1a65e352fa986b0674e24b7495845dd63d
Felipe Ortiz <fortizc@gmail.com>
2019-10-30 01:23:26 -0300

    Add InputWidget class

---
+++ src/input_widget.h
@@ -0,0 +1,23 @@
+#ifndef BLACK_BOARD_INPUT_WIDGET_H
+#define BLACK_BOARD_INPUT_WIDGET_H
+
+#include <gtkmm/scrolledwindow.h>
+
+#include <memory>
+
+namespace blackboard
+{
+    class InputWidget : public Gtk::ScrolledWindow
+    {
+    public:
+        InputWidget();
+        virtual ~InputWidget();
+        Glib::RefPtr<Gtk::TextBuffer> get_buffer();
+
+    private:
+        class Impl;
+        std::unique_ptr<Impl> pimpl;
+    };
+}
+
+#endif
