6120cde7128e5955467268e1df3c3ddf56072b35
Felipe Ortiz <fortizc@gmail.com>
2019-11-02 04:37:50 -0300

    Add VarProcessor class

---
+++ src/var_processor.h
@@ -0,0 +1,31 @@
+#ifndef BLACK_BOARD_VAR_PROCESSOR_H
+#define BLACK_BOARD_VAR_PROCESSOR_H
+
+#include <memory>
+#include <unordered_map>
+#include <string>
+
+namespace blackboard
+{
+    class VarProcessor
+    {
+    public:
+        using VarList = std::unordered_map<std::string, std::string>;
+
+        VarProcessor();
+        ~VarProcessor();
+
+        void update(const VarList &vars);
+        void add(const std::string &name, const std::string &value);
+        void replace(std::string &cmd);
+        void erase(const std::string &name);
+
+        VarList &get_vars() const;
+
+    private:
+        class Impl;
+        std::unique_ptr<Impl> pimpl;
+    };
+}
+
+#endif
