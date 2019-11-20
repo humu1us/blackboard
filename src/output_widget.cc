0b43538be2dc581312a3885cee91bcab8303e086
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2019-11-20 13:04:56 -0300

    Some improvements made by @fortizc

--- src/output_widget.cc
+++ src/output_widget.cc
@@ -2,8 +2,6 @@
 
 #include <gtkmm/textview.h>
 
-#include <memory>
-
 namespace bb
 {
     class OutputWidget::Impl
@@ -15,28 +13,26 @@ namespace bb
         void delete_line(int line);
 
         Gtk::TextView tv;
-        Glib::RefPtr<Gtk::TextBuffer> buffer;
+        Gtk::TextBuffer *buffer;
     };
 }
 
 // Internal implementation
 
 bb::OutputWidget::Impl::Impl()
+    : buffer(tv.get_buffer().get())
 {
     tv.set_can_focus(false);
-    buffer = tv.get_buffer();
 }
 
 void bb::OutputWidget::Impl::set_line(int line, const std::string &text)
 {
     int last_line = buffer->get_line_count() - 1;
     if (line > last_line)
-    {
         create_empty_lines(line - last_line);
-    }
+
     delete_line(line);
-    auto iter = buffer->get_iter_at_line(line);
-    buffer->insert(iter, text);
+    buffer->insert(buffer->get_iter_at_line(line), text);
 }
 
 void bb::OutputWidget::Impl::create_empty_lines(int n)
@@ -44,16 +40,14 @@ void bb::OutputWidget::Impl::create_empty_lines(int n)
     std::string new_lines(n * 2, ' ');
     for (int i = 1; i < n * 2; i += 2)
         new_lines[i] = '\n';
-    auto iter = buffer->end();
-    buffer->insert(iter, new_lines);
+    buffer->insert(buffer->end(), new_lines);
 }
 
 void bb::OutputWidget::Impl::delete_line(int line)
 {
-    auto beg_iter = buffer->get_iter_at_line(line);
-    auto end_iter = beg_iter;
-    end_iter.forward_to_line_end();
-    buffer->erase(beg_iter, end_iter);
+    auto end = buffer->get_iter_at_line(line);
+    end.forward_to_line_end();
+    buffer->erase(buffer->get_iter_at_line(line), end);
 }
 
 // Public API
